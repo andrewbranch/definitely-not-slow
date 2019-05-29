4dad603f170df339c7b908cd798721b2bf1a2d28
Andrew Branch <andrew@wheream.io>
2019-05-28 17:37:40 -0700

    Update dist

--- dist/src/cli/compareTypeScript.d.ts
+++ dist/src/cli/compareTypeScript.d.ts
@@ -1,12 +1,18 @@
 import { PackageId } from "types-publisher/bin/lib/packages";
-import { Args } from "../common";
+import { PackageBenchmarkSummary, Args, JSONDocument } from "../common";
 export interface CompareTypeScriptOptions {
     compareAgainstMajorMinor: string;
     definitelyTypedPath: string;
     packages?: PackageId[];
     maxRunSeconds?: number;
     typeScriptPath?: string;
+    outFile?: string;
+    groups?: {
+        [key: string]: JSONDocument<PackageBenchmarkSummary>;
+    }[];
+    agentCount?: number;
+    agentIndex?: number;
     upload: boolean;
 }
 export declare function compareTypeScriptCLI(args: Args): Promise<void>;
-export declare function compareTypeScript({ compareAgainstMajorMinor, definitelyTypedPath, packages, maxRunSeconds, typeScriptPath, upload, }: CompareTypeScriptOptions): Promise<void>;
+export declare function compareTypeScript({ compareAgainstMajorMinor, definitelyTypedPath, packages, maxRunSeconds, typeScriptPath, upload, outFile, groups, ...opts }: CompareTypeScriptOptions): Promise<void>;
