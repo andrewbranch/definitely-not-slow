17826c289d51f657cf7277fa5028a264c4f08fc7
Andrew Branch <andrew@wheream.io>
2019-05-31 15:50:57 -0700

    Update dist

--- dist/src/github/metrics.d.ts
+++ dist/src/github/metrics.d.ts
@@ -1,17 +1,19 @@
 import { PackageBenchmarkSummary, Document } from '../common';
 export interface FormatOptions {
-    noDiff?: boolean;
     precision?: number;
     indent?: number;
-    higherIsBetter?: boolean;
-    includeEmoji?: boolean;
+}
+export declare const enum SignificanceLevel {
+    Warning = "warning",
+    Alert = "alert",
+    Awesome = "awesome"
 }
 export interface Metric {
     columnName: string;
     sentenceName: string;
-    isUninteresting?: boolean;
     formatOptions?: FormatOptions;
     getValue: (x: Document<PackageBenchmarkSummary>) => number | undefined;
+    getSignificance: (percentDiff: number, before: Document<PackageBenchmarkSummary>, after: Document<PackageBenchmarkSummary>) => SignificanceLevel | undefined;
 }
 export declare type MetricName = 'typeCount' | 'assignabilityCacheSize' | 'subtypeCacheSize' | 'identityCacheSize' | 'samplesTaken' | 'identifierCount' | 'completionsMean' | 'completionsMedian' | 'completionsStdDev' | 'quickInfoMean' | 'quickInfoMedian' | 'quickInfoStdDev' | 'completionsWorstMean' | 'quickInfoWorstMean';
 export declare const metrics: {
